3faec9d1998139ab1cbab0f69014c55099b5e089
enderduque1758-png <enderduque1758@gmail.com>
2026-09-12 23:05:27 -0500

    Mount global market radar

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GlobalMarketMonitor from './GlobalMarketMonitor';
 import './index.css';
 import './alerts.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalMarketMonitor />
     <App />
   </StrictMode>,
 );
